4d87c69a59a23ccd13a96ba12ae293ccd4cfc692
PaaRich <siehrichmond50@gmail.com>
2024-11-19 11:43:25 +0000

    package installation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // For Vite projects
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust based on your file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
