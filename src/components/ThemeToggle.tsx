65e9716cf22b1c92e1aa8d7fe0629e1d22a2a9bc
PaaRich <siehrichmond50@gmail.com>
2024-11-20 03:57:01 +0000

    Toggle comp

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,12 @@
+import { IoIosSunny, IoMdMoon } from "react-icons/io";
+
+const ThemeToggle = () => {
+  return (
+    <div className="flex items-center p-1 rounded-full">
+      <IoIosSunny />
+      <IoMdMoon />
+    </div>
+  );
+};
+
+export default ThemeToggle;
