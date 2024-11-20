be6282bf104c933ef29284cc1447f458f121a65b
PaaRich <siehrichmond50@gmail.com>
2024-11-20 03:55:07 +0000

    Implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Transactions from "./pages/Transactions";
+import Settings from "./pages/Settings";
+import Research from "./pages/Research";
+import Report from "./pages/Report";
+import Team from "./pages/Team";
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="transactions" element={<Transactions />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="research" element={<Research />} />
+          <Route path="report" element={<Report />} />
+          <Route path="team" element={<Team />} />
+        </Route>
+      </Routes>
     </>
   );
 }
