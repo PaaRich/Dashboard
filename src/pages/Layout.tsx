70df236df9220ff8cf85c7baffba1ad1e260b11a
PaaRich <siehrichmond50@gmail.com>
2024-11-20 03:48:36 +0000

    Implement pages

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+
+const Layout = () => {
+  return (
+    <div className="flex">
+      <div className="basis-1/5 ">
+        <Sidebar />
+      </div>
+
+      <div className="basis-4/5">
+        <Navbar />
+        <div className="overflow-auto bg-[#bbbbf557] ">
+          <div className="max-w-[95%] mx-auto">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
