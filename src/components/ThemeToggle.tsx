58e2a7ad804d2c9ec7a062b5fbd048d45a8cde10
PaaRich <siehrichmond50@gmail.com>
2024-11-21 08:43:54 +0000

    toggle theme implemented

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -2,9 +2,9 @@ import { IoIosSunny, IoMdMoon } from "react-icons/io";
 
 const ThemeToggle = () => {
   return (
-    <div className="flex items-center p-1 rounded-full">
-      <IoIosSunny />
-      <IoMdMoon />
+    <div className="flex items-center p-2 rounded-full bg-slate-50 w-16 mr-4">
+      <IoIosSunny size={24} className="text-slate-400" />
+      <IoMdMoon size={24} />
     </div>
   );
 };
