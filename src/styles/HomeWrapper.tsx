c442ced7bc60a477b0bdf54afaf0ec441e654945
PaaRich <siehrichmond50@gmail.com>
2024-11-24 07:54:05 +0000

    HomeWrapper

---
+++ src/styles/HomeWrapper.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const HomeWrapper = styled.section`
+  background-color: #ededed;
+  padding: 1rem 0;
+
+  .c-cards {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 1rem;
+  }
+`;
+
+export const Card = styled.div``;
