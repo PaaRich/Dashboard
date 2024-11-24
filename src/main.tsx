4a581b8029c7dcbb402be42a2b627c748e2de1ee
PaaRich <siehrichmond50@gmail.com>
2024-11-24 07:41:23 +0000

    Added AppContext

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import AppContext from "./Context/AppContext.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppContext>
+        <App />
+      </AppContext>
     </BrowserRouter>
   </StrictMode>
 );
