01cd46b7cecb89a4b571337c5960bb623fa54c69
PaaRich <siehrichmond50@gmail.com>
2024-11-24 07:55:31 +0000

    Sidebar hover effects

--- src/styles/Sidebar.styled.tsx
+++ src/styles/Sidebar.styled.tsx
@@ -1,22 +1,22 @@
 import styled from "styled-components";
 
 export const SidebarWrapper = styled.div`
-  height: 100dvh;
-
   & ul li {
     display: flex;
     align-items: center;
+    padding: 0.25rem 0;
   }
 
   & .active {
     color: white;
-    background-color: #6f6fef;
+    background-color: #3b82f6;
   }
   & ul li a {
     transition-duration: 0.5s;
   }
   & ul li a:not(.active):hover {
     transform: translateX(0.5rem);
+    background-color: whitesmoke;
     color: #1a1acf;
   }
 `;
