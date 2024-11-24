a6225526209296af655b01565dd789284ba1d92e
PaaRich <siehrichmond50@gmail.com>
2024-11-24 07:53:22 +0000

    context api

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,20 +1,35 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
+import { Context } from "../Context/Context";
+import { useContext } from "react";
+import { IoClose } from "react-icons/io5";
+//import { LayoutWrapper } from "../styles/LayoutWrapper";
 
 const Layout = () => {
+  const { isOpen, setIsOpen } = useContext(Context);
   return (
-    <div className="flex">
-      <div className="basis-1/5 ">
+    <div className="relative md:flex lg:relative">
+      <div
+        className={`bg-white md:basis-1/5 lg:fixed lg:w-1/5 lg:left-0 md:relative z-50 md:-left-0 shadow-lg md:shadow-none  absolute duration-300 ${
+          !isOpen ? "left-0" : "-left-full"
+        }`}
+      >
         <Sidebar />
+        <IoClose
+          className="absolute top-0 left-full bg-red-200 md:hidden"
+          size={25}
+          onClick={() => setIsOpen(true)}
+        />
       </div>
 
-      <div className="basis-4/5">
-        <Navbar />
-        <div className="overflow-auto bg-[#cdcdf454] ">
-          <div className="max-w-[95%] mx-auto">
-            <Outlet />
-          </div>
+      <div className="md:basis-4/5 lg:w-4/5">
+        <div className="shadow lg:fixed lg:right-0 lg:w-4/5 z-40 lg:h-[68px]">
+          <Navbar />
+        </div>
+
+        <div className="lg:absolute lg:right-0 lg:w-4/5 lg:top-[68px]">
+          <Outlet />
         </div>
       </div>
     </div>
