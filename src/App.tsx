cd832c4e38145330cc32d321d7286edd59040808
SIEH RICHMOND <siehrichmond50@gmail.com>
2024-11-26 14:43:16 +0000

    added projectdetail file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import ProjectDetail from "./pages/ProjectDetail";
 import Transactions from "./pages/Transactions";
 import Settings from "./pages/Settings";
 import Research from "./pages/Research";
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="projects" element={<Projects />} />
+          <Route path="projects/:id" element={<ProjectDetail/>}/>
           <Route path="transactions" element={<Transactions />} />
           <Route path="settings" element={<Settings />} />
           <Route path="research" element={<Research />} />
