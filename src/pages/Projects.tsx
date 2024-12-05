2fe09758cf363786bd1c965824167e16049b306a
PaaRich <siehrichmond50@gmail.com>
2024-12-05 06:14:59 +0000

    FileInput

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,31 +1,32 @@
-import Project from "../components/Project"
+import Project from "../components/Project";
 import FileInput from "../components/FileInput";
 
 const Projects = () => {
-  return <div className="max-w-[95%] mx-auto py-5">
-    <div className="grid grid-cols-3 gap-4">
-    <FileInput/>
-    <Project 
-      name="Lorem, ipsum dolor." 
-      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, commodi!"
-      date="11/12/2020"
-      rocall="3"
-      />
-      <Project 
-      name="Lorem, ipsum dolor." 
-      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, commodi!"
-      date="11/12/2020"
-      rocall="3"
-      />
-      <Project 
-      name="Lorem, ipsum dolor." 
-      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, commodi!"
-      date="11/12/2020"
-      rocall="3"
-      />
+  return (
+    <div className="max-w-[95%] mx-auto py-5">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4">
+        <FileInput />
+        <Project
+          name="Lorem, ipsum dolor."
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, commodi!"
+          date="11/12/2020"
+          rocall="3"
+        />
+        <Project
+          name="Lorem, ipsum dolor."
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, commodi!"
+          date="11/12/2020"
+          rocall="3"
+        />
+        <Project
+          name="Lorem, ipsum dolor."
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, commodi!"
+          date="11/12/2020"
+          rocall="3"
+        />
+      </div>
     </div>
-    
-  </div>;
+  );
 };
 
 export default Projects;
