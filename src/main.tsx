b857884b430c457ad2f9199080b19ae204655fef
PaaRich <siehrichmond50@gmail.com>
2024-12-05 06:10:14 +0000

    import slick-carousel

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import AppContext from "./Context/AppContext.tsx";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
