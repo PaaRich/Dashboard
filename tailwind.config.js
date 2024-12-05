3628910588e7a86a8aea7a60bfe2d82ec03b0b4c
PaaRich <siehrichmond50@gmail.com>
2024-12-05 06:08:43 +0000

    added animation to extend obj

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        heartbeat: "heartbeat 1.5s ease-in-out infinite",
+      },
+      keyframes: {
+        heartbeat: {
+          "0%, 100%": { transform: "scale(1)" },
+          "30%": { transform: "scale(1.3)" },
+          "60%": { transform: "scale(1.1)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
