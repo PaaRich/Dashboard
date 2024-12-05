33188a9f73a43d5fb63d590e183d8d366cce42d3
PaaRich <siehrichmond50@gmail.com>
2024-12-05 06:15:23 +0000

    Settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,15 @@
+import ProfilePictureUpload from "../components/ProfilePic";
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div>
+      <div className="max-w-[80%] mx-auto py-5">
+        <h1 className="text-xl font-semibold">Account</h1>
+        <div>
+          <ProfilePictureUpload />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
