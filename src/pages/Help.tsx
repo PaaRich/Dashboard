3000b19fd272aeb25009078d9409143bdc7e6c29
PaaRich <siehrichmond50@gmail.com>
2024-12-05 06:13:45 +0000

    implemented accordion

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,5 +1,127 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+import { FaMessage } from "react-icons/fa6";
 const Help = () => {
-  return <div>Help</div>;
+  return (
+    <div className="lg:w-[70%] mx-auto py-8 bg-white">
+      <h1 className="font-semibold text-3xl text-center mb-4">
+        Frequently Asked Questions
+      </h1>
+      <Accordion allowZeroExpanded>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              What harsh truths do you prefer to ignore?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
+              occaecat ut occaecat consequat est minim minim esse tempor laborum
+              consequat esse adipisicing eu reprehenderit enim.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              What harsh truths do you prefer to ignore?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
+              occaecat ut occaecat consequat est minim minim esse tempor laborum
+              consequat esse adipisicing eu reprehenderit enim.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              What harsh truths do you prefer to ignore?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
+              occaecat ut occaecat consequat est minim minim esse tempor laborum
+              consequat esse adipisicing eu reprehenderit enim.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              Is free will real or just an illusion?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+              nostrud velit in irure cillum tempor laboris sed adipisicing eu
+              esse duis nulla non.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              What harsh truths do you prefer to ignore?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
+              occaecat ut occaecat consequat est minim minim esse tempor laborum
+              consequat esse adipisicing eu reprehenderit enim.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              What harsh truths do you prefer to ignore?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
+              occaecat ut occaecat consequat est minim minim esse tempor laborum
+              consequat esse adipisicing eu reprehenderit enim.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              What harsh truths do you prefer to ignore?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
+              occaecat ut occaecat consequat est minim minim esse tempor laborum
+              consequat esse adipisicing eu reprehenderit enim.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+      </Accordion>
+      <div className="fixed bottom-10 right-5 bg-white p-3 rounded-full cursor-pointer group">
+        <FaMessage
+          size={25}
+          color="blue"
+          className="animate-heartbeat group-hover:animate-none"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Help;
