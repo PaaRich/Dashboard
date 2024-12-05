2b70af9a47ed54f65aefd4da19845d98f5d6c97a
PaaRich <siehrichmond50@gmail.com>
2024-12-05 06:10:37 +0000

    types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,18 @@
-export interface ProjectDetailProp{
-    name:string;
-    description:string;
-    date:string;
-    rocall:string;
-    
+export interface ProjectDetailProp {
+  name: string;
+  description: string;
+  date: string;
+  rocall: string;
+}
+export interface Transaction {
+  name: string;
+  status: string;
+  date: string;
+  amount: number;
+}
+
+export interface PersonnelProp {
+  img: string;
+  name: string;
+  position: string;
 }
